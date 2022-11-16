1a491d6e752b26554b25d7505138faea45e0560b
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2022-11-16 22:53:45 +0200

    1

---
+++ src/21-index.js
@@ -0,0 +1,26 @@
+import './css/common.css';
+
+import './js/c - post';
+import './js/u - patch';
+import './js/d - delete';
+
+// Пакет json-server https://github.com/typicode/json-server
+// Postman/Postwoman/Insomnia и т. д.
+// Работа с приватным API
+// Create
+// Метод POST
+// Тело
+// Заголовки
+// Ответ
+// Read
+// Метод GET
+// Ответ
+// Update
+// Методы PUT и PATCH
+// Тело
+// Заголовки
+// Ответ
+// Delete
+// Метод DELETE
+// Ответ
+// Цикл запрос-ответ
